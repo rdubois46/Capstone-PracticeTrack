1358b5bb4d38c8c3665a01b80acf13e7e6df8d53
Ryan DuBois <ryan-dubois@utulsa.edu>
2019-08-03 17:10:05 -0500

    Added Spring session dependencies, configured application.properties to use database, and created User class

--- capstone-practice-track/src/main/java/org/launchcode/capstonepracticetrack/models/User.java
+++ capstone-practice-track/src/main/java/org/launchcode/capstonepracticetrack/models/User.java
@@ -1,4 +1,69 @@
 package org.launchcode.capstonepracticetrack.models;
 
+
+import com.sun.istack.internal.NotNull;
+import org.hibernate.validator.constraints.Email;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Entity
 public class User {
+
+    @GeneratedValue
+    @Id
+    private int id;
+
+    @NotNull
+    @Size(min = 3, max = 15)
+    private String username;
+
+    @Email
+    private String email;
+
+    @NotNull
+    @Size(min = 5, message = "Password must be at least 5 characters.")
+    private String password;
+
+    public User () {
+
+    }
+
+    public User (String username, String password) {
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
+
+
+
